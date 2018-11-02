046297913eb063de67f6d0c81cdc55ffb02c7528
Perfspec <lewisrwebb@gmail.com>
2018-11-02 11:48:03 +0000

    added names to entities and input variables to the OneToMany tag

--- classroom/src/main/java/com/qa/persistence/domain/Trainee.java
+++ classroom/src/main/java/com/qa/persistence/domain/Trainee.java
@@ -1,15 +1,17 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "trainee")
 public class Trainee {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Long id;
+	@Column(length = 50)
 	private String traineeName;
 	
 	public Trainee() {}
